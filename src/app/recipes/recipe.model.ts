e0063deaaaecd2b4933c5510cf45b84abd46e035
thento123 <themistoklidukoli@outlook.com>
2020-12-19 22:29:41 +0100

    second

--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -1,8 +1,11 @@
+//its a class object which helps us to define the structure of our recipe data that we will access 
+
 export class Recipe{
     public name:string;
     public description:string;
     public imagePath:string;
 
+    //the constructor will help us access this object from other files
     constructor(name:string, desc:string, imagePath:string){
         this.name=name;
         this.description=desc;
---
+++ src/app/share/ingredient.model.ts
@@ -0,0 +1,10 @@
+//this is created in an another folder is it will be accessed by both of our features (shopping list and recipe)
+
+export class Ingredient{
+    //public name:string;
+    //public amount:number;
+
+    constructor(public name:string, public amount:number){
+
+    }
+}
